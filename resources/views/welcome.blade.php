b97b6a67e5a18181237c86a5aabfd537411a234d
fatima208 <133785043+fatima208@users.noreply.github.com>
2025-01-08 15:31:17 +0500

    bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>CRUD</title>
         <link rel="stylesheet" href="css/style.css">
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
     <video id="VideoBackground" preload="auto" autoplay loop muted>
